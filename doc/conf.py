2b7afd8d333ce77b291a45b7e1b8f8f6f7304cb4
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-08-13 14:35:36 +0200

    Add sphinx.ext.githubpages
    This basically adds a .nojekyll file automatically which is needed when
    deploying to GitHub Pages.

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.intersphinx", "sphinx.ext.todo", "sphinx.ext.mathjax"]
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
